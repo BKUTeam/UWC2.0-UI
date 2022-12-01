a8d0b4c07b2e45f76b7e43710b583b5144aa8821
Phan Hải Hà <haihaphan626@gmail.com>
2022-12-01 17:17:25 +0700

    feat/header: create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import Styles from './header.module.scss';
+
+function RenderToday() {
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+    let today = new Date();
+    let day = weekday[today.getDay()];
+    let date = today.getDate();
+    let month = months[today.getMonth()];
+    let year = today.getFullYear();
+
+    return `${day}, ${month} ${date}, ${year}`;
+}    
+
+export default function Header() {
+    return (
+        <>
+            <header>
+                <div className={Styles.first}>UWC 2.0 | Destroy the world</div>
+                <div className={Styles.second}>
+                    <div className={Styles['left-text']}>
+                        <i className="fa-solid fa-circle-info me-2 d-inline"></i>
+                        <div className="d-inline">You have 7 filled MCPs</div>
+                    </div>
+                </div>
+                <div className={Styles.third}>
+                    <div className={Styles['right-text']}>
+                        <Button className='d-inline'>Janitor</Button>
+                        <div className='d-inline ms-2'>{RenderToday()}</div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}

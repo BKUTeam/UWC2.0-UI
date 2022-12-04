2f05bcab91d371e637fc869c26480bdd88469e52
hnimtadd <minhdat15012002@gmail.com>
2022-12-04 19:55:05 +0700

    temp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import classNames from 'classnames/bind';
 import { Map } from '~/components/Map';
 import styles from '~/components/GlobalStyles/GlobalStyles.module.scss';
-import { useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Header from '~/components/Header/Header';
 import RightSideBar from '~/components/RightSideBar/Collector';
-import { routeData as rData, markerData as mData } from '~/components/Map/data';
+
+export const MapContext = createContext();
 const cx = classNames.bind(styles);
 function App() {
     const [routeData, setRouteData] = useState([]);
@@ -24,24 +25,21 @@ function App() {
                 setdepotInfo(data);
             });
     }, []);
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setMarkerData(mData.features);
-    //     }, 3000);
-    // }, []);
 
     return (
         <div className={cx('app')}>
+            <div className={cx('header')}>{<Header />}</div>
             <div className={cx('sidenav', 'left-sidenav')}>Left sideNav</div>
             <div className={cx('content')}>
-                <div className={cx('header')}>{<Header />}</div>
-                <div className={cx('sidenav', 'right-sidenav')}>
-                    <RightSideBar />
-                </div>
                 <div className={cx('main-content')}>
-                    <Map routeData={routeData} markerData={[...mcpInfo, ...depotInfo]} />
+                    <MapContext.Provider value={{ routeContext: routeData, markerContext: [...mcpInfo, ...depotInfo] }}>
+                        <Map />
+                    </MapContext.Provider>
                 </div>
                 <div className={cx('footer')}>Footer</div>
+            </div>{' '}
+            <div className={cx('sidenav', 'right-sidenav')}>
+                <RightSideBar />
             </div>
         </div>
     );
